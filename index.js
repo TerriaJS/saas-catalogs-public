87838a6d00a9ee64fe8103cd23837495035ab211
Nick Forbes-Smith <nick.forbes-smith@data61.csiro.au>
2021-05-26 21:12:37 +1000

    another test

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ try {
   core.setOutput("test", process.env.CHANGED_FILES);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2);
-  console.log(`The event payload: ${payload}`);
+  console.log(`The event payload (test): ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
 }
