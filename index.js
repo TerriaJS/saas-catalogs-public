ee96aae7f537adf059d89712214bbf109c88dd4d
Nick Forbes-Smith <nick.forbes-smith@data61.csiro.au>
2021-05-26 21:08:04 +1000

    ci test

--- index.js
+++ index.js
@@ -2,10 +2,9 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 
 try {
-  const changedFiles = core.getInput("changed-files");
-  console.log(changedFiles);
+  console.log(process.env.CHANGED_FILES);
 
-  core.setOutput("test", changedFiles);
+  core.setOutput("test", process.env.CHANGED_FILES);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2);
   console.log(`The event payload: ${payload}`);
