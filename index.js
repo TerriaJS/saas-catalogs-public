d1c0f12f47dc81d8ee3ae2db56e0391dfa421761
Nick Forbes-Smith <nick.forbes-smith@data61.csiro.au>
2021-05-26 21:34:15 +1000

    test

--- index.js
+++ index.js
@@ -2,10 +2,8 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 
 try {
-  console.log(process.env.ADDED_FILES);
-  console.log(process.env.CHANGED_FILES);
-  // const addedFiles = JSON.parse(process.env.ADDED_FILES);
-  // const changedFiles = JSON.parse(process.env.CHANGED_FILES);
+  const addedFiles = process.env.ADDED_FILES?.join(",") ?? [];
+  const changedFiles = process.env.CHANGED_FILES?.join(",") ?? [];
 
   console.log(addedFiles);
   console.log(changedFiles);
